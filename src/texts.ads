8ecad80e08ba938771b75922059024f94e0901b6
Alejandro R. Mosteo <amosteo@unizar.es>
2024-11-14 07:35:19 +0100

    Demo subcrate

--- src/texts.ads
+++ src/texts.ads
@@ -1,6 +1,14 @@
+private with Ada.Strings.Wide_Wide_Unbounded;
+
 package Texts with Preelaborate is
 
    type Text is tagged private;
    --  A Unicode text
+
+private
+
+   type Text is tagged record
+      Str : Ada.Strings.Wide_Wide_Unbounded.Unbounded_Wide_Wide_String;
+   end record;
 
 end Texts;
